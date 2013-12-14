106bbeb7aaf32e0424e505e5f23576b79c97700e
Ray Shan <ray@rayshan.com>
2013-12-14 15:34:14 -0800

    initial flex box stuff

--- web/p-grid.dart
+++ web/p-grid.dart
@@ -6,5 +6,7 @@ class PGrid extends PolymerElement {
   final List fruits = toObservable(['apples', 'pears', 'bananas']);
 
   PGrid.created() : super.created();
+
+  bool get applyAuthorStyles => true;
 }
 
