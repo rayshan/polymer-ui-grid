359ccbd1a73b5dc65b051ffcc381221fa2d0f65f
Charles d'Harcourt <charles@dharcourt.com>
2013-12-14 15:28:19 -0800

    Fetch data from JSON file using polymer-ajax.

--- web/p-grid.dart
+++ web/p-grid.dart
@@ -3,8 +3,12 @@ import 'package:polymer/polymer.dart';
 
 @CustomTag('p-grid')
 class PGrid extends PolymerElement {
-  final List fruits = toObservable(['apples', 'pears', 'bananas']);
+  @observable List people;
 
   PGrid.created() : super.created();
+
+  dataReceived(e, detail, node) {
+    this.people = detail['response'];
+  }
 }
 
