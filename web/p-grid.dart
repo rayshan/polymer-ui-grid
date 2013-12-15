61cf53de7bcfa4e7c8157f25648b7da7f81a3b60
Natasha Hull <musicalnat@gmail.com>
2013-12-14 20:34:06 -0800

    Finished basic sorting

--- web/p-grid.dart
+++ web/p-grid.dart
@@ -4,19 +4,25 @@ import 'dart:html';
 @CustomTag('p-grid')
 class PGrid extends PolymerElement {
   @published List columns;
-  @published List rows;
+  @published @observable List rows;
   @observable List sorted;
   
   PGrid.created() : super.created() {
     this.sorted = ["id", "increasing"];
   }
-  
+
   sortTable(Event e, var detail, Node target) {
     // Need to account for Active? to isActive
     String sortByEl = target.text.split(' ')[0];
     sortByEl = sortByEl.toLowerCase();
-    print(sortByEl);
-    print(this.rows..sort((a,b) => a[sortByEl].compareTo(b[sortByEl])));
+    this.rows = new List.from(this.rows); //So it knows it's changed
+    if(sorted[0] == sortByEl && sorted[1] == "increasing") {
+      this.rows = this.rows..sort((a,b) => b[sortByEl].compareTo(a[sortByEl]));
+      this.sorted = [sortByEl, "decreasing"];
+    } else {
+      this.rows = this.rows..sort((a,b) => a[sortByEl].compareTo(b[sortByEl]));
+      this.sorted = [sortByEl, "increasing"];
+    }
   }
 }
 
