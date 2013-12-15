9137988d02f5d1e95b5affb61bc7b5edb5d950bf
Natasha Hull <musicalnat@gmail.com>
2013-12-14 18:32:54 -0800

    Started working on sorting

--- web/p-grid.dart
+++ web/p-grid.dart
@@ -1,9 +1,20 @@
 import 'package:polymer/polymer.dart';
+import 'dart:html';
 
 @CustomTag('p-grid')
 class PGrid extends PolymerElement {
   @published List columns;
   @published List rows;
-  PGrid.created() : super.created();
+  @observable List sorted;
+  
+  PGrid.created() : super.created() {
+    this.sorted = ["id", "increasing"];
+  }
+  
+  sortTable(Event e, var detail, Node target) {
+    String sortByEl = target.text.split(' ')[0];
+    sortByEl = sortByEl.toLowerCase();
+    this.rows.sort((a,b) => a[sortByEl].compareTo(b[sortByEl]));
+  }
 }
 
